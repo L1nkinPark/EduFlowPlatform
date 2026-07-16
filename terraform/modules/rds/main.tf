096f1048feb340975e47030986b3407013a5f5ae
L1nkinPark <hieuvdfs91539@fpt.edu.vn>
2026-07-16 20:08:07 +0700

    Set apply_immediately = true for RDS instance to force database configuration changes instantly

--- terraform/modules/rds/main.tf
+++ terraform/modules/rds/main.tf
@@ -92,6 +92,7 @@ resource "aws_db_instance" "main" {
   db_subnet_group_name   = aws_db_subnet_group.main.name
   vpc_security_group_ids = [var.rds_security_group_id]
   publicly_accessible    = false
+  apply_immediately      = true
 
   # High Availability
   multi_az = var.multi_az
